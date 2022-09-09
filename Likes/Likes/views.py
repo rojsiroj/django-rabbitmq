e37ceb546ad1cc34406ef228bd749137da96aae5
rojsiroj <sirojudin.dev@gmail.com>
2022-09-09 18:38:08 +0700

    initial commit

---
+++ Likes/Likes/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework import mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import *
+from .serializers import *
+
+
+class QuoteViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = QuoteSerializer
+    queryset = Quote.objects.all()
+
+
+class QuoteUserViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = QuoteUserSerializer
+    queryset = QuoteUser.objects.all()
