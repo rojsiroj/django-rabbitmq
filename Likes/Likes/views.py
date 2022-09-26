7ee3cc4dfebf6beef9040e0dc26fece18d5a449f
rojsiroj <sirojudin.dev@gmail.com>
2022-09-26 09:31:22 +0700

    create likes consumer

--- Likes/Likes/views.py
+++ Likes/Likes/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,6 +8,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import *
+from .producer import publish
 from .serializers import *
 
 
@@ -18,3 +20,25 @@ class QuoteViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.Create
 class QuoteUserViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
     serializer_class = QuoteUserSerializer
     queryset = QuoteUser.objects.all()
+
+
+@api_view(['GET'])
+def like(request, pk, format=None):
+
+    query = {'username': 'john'}
+    req = requests.get('http://127.0.0.1:8000/users', params=query)
+    data = req.json()
+    print(data)
+
+    try:
+        for s in range(len(data)):
+            if data[s]['id']:
+                quoteuser = QuoteUser.objects.create(
+                    user_id=data[s]['id'], quote_id=pk)
+                quoteuser.save()
+                publish('quote_liked', pk)
+                print('Quoteuser created')
+                return Response('Quote liked...', status=status.HTTP_201_CREATED)
+    except:
+
+        return Response("Quote liked...", status=status.HTTP_400_BAD_REQUEST)
--- Likes/consumer.py
+++ Likes/consumer.py
@@ -1,15 +1,21 @@
-from likes.models import Quote
+# from likes.models import Quote
 import json
 import pika
-import django
+# import django
+# from django.conf import settings
 from sys import path
 from os import environ
+# from likes import defaults
+# from likes import Settings
 
 
 # Your path to settings.py file
 # path.append(
 #     '/Users/rojsiroj/Projects/sandboxes/python/rabbitmq/Likes/likes/settings.py')
 # environ.setdefault('DJANGO_SETTINGS_MODULE', 'likes.settings')
+# settings = Settings(apps=["my_app"])
+# settings = Settings(modules=[defaults])
+# settings.configure(default_settings=settings, DEBUG=True)
 # django.setup()
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
@@ -25,17 +31,17 @@ def callback(ch, method, properties, body):
     print(data)
 
     if properties.content_type == 'quote_created':
-        quote = Quote.objects.create(id=data['id'], title=data['title'])
-        quote.save()
+        # quote = Quote.objects.create(id=data['id'], title=data['title'])
+        # quote.save()
         print("quote created")
     elif properties.content_type == 'quote_updated':
-        quote = Quote.objects.get(id=data['id'])
-        quote.title = data['title']
-        quote.save()
+        # quote = Quote.objects.get(id=data['id'])
+        # quote.title = data['title']
+        # quote.save()
         print("quote updated")
     elif properties.content_type == 'quote_deleted':
-        quote = Quote.objects.get(id=data)
-        quote.delete()
+        # quote = Quote.objects.get(id=data)
+        # quote.delete()
         print("quote deleted")
 
 
